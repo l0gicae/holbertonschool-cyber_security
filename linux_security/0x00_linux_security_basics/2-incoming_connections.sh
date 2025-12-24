71f95857b2025f18797cb39cb027f97da4847f48
Farid Mehtizada <107333073+l0gicae@users.noreply.github.com>
2025-12-24 10:05:04 +0400

    Update 2-incoming_connections.sh

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ufw default deny incoming && ufw default allow outgoing && ufw allow 80/tcp && ufw --force enable && echo "Rules updated" && ufw6 default deny incoming && ufw6 default allow outgoing && ufw6 allow 80/tcp && echo "Rules updated (v6)"
+sudo ufw allow 80/tcp 
